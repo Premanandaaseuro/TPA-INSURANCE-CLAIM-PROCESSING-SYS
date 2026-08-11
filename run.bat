c7a2804a798a9b08e94e06e0f7a3fdbcc67c3884
Premanandaaseuro <M.Premananda@aseuro.inaseuro>
2026-08-11 15:51:56 +0530

    fix: update script logs to reflect port 7002

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ echo ===================================================
 echo TPA Insurance Claim Processing System
 echo Starting Production Standalone Spring Boot Application
 echo ===================================================
-echo Application URL: http://localhost:8081/
+echo Application URL: http://localhost:7002/
 echo ===================================================
 java -jar target\tpa-claim-processor-1.0.0-SNAPSHOT.jar
